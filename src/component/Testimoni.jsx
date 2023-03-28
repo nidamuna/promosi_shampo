653debfa6265bf8c7c06949d5e630568f18d6d6a
nidamuna <munanida@gmail.com>
2023-03-28 10:07:31 +0700

    rsponsive testimoni sm

--- src/component/Testimoni.jsx
+++ src/component/Testimoni.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Foto from '../assets/testi-foto.png'
+import Foto2 from '../assets/testi-sm.png'
 
 
 const Testimoni = () => {
@@ -10,12 +11,13 @@ const Testimoni = () => {
                 <div
                   className="absolute transitiona-all duration-1000 opacity-70 inset-0 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200">
                 </div>
-                <h1 className='relative inline-flex font-bold text-4xl w-full px-14'>
+                <h1 className='relative inline-flex font-bold text-2xl md:text-4xl w-full px-0 md:px-14'>
                     Sudah banyak yang membuktikan kualitas AW Premium Polish
                 </h1>
             </div>
            
-            <img src={Foto} alt="hasil" />
+            <img src={Foto} alt="hasil" className='md:flex hidden' />
+            <img src={Foto2} alt="hasil" className='flex md:hidden' />
         </section>
     </>  
 )
